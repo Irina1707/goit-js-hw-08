9bc21f3da974cc59b70d9bb6b621e2a247bbbd8d
Irina1707 <irina_niv@mail.ru>
2022-01-16 12:06:07 +0300

    изменяет feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,14 +1,15 @@
 import throttle from 'lodash.throttle';
 
 
-const formData = {};
-
 const formEl = document.querySelector('.feedback-form');
 
 formEl.addEventListener('submit', onFormSubmit);
 
 formEl.addEventListener('input', throttle(onInput, 500));
 
+
+
+ 
 function onInput(event) {
     const name = event.target.name;
     const value = event.target.value;
@@ -18,27 +19,34 @@ function onInput(event) {
     localStorage.setItem('feedback-form-state', JSON.stringify(formData));
 }
 
-populateForm()
+populateForm();
+
+let formData = {};
+if (localStorage.getItem("feedback-form-state")) {
+    formData =  JSON.parse(localStorage.getItem("feedback-form-state"))
+} 
 
 function onFormSubmit(event) {
     event.preventDefault();
 
-   
-    populateForm()
     event.currentTarget.reset();
+
+    if (localStorage.getItem('feedback-form-state')) {
+        console.log(formData);
+    }
     localStorage.removeItem('feedback-form-state');
-    
- } 
+    //console.log(formData);
+    formData = {};
+ }
 
 function populateForm() {
     const savedForm = localStorage.getItem('feedback-form-state');
 
-    if (savedForm) {
+         if (savedForm) {
         const formState = JSON.parse(savedForm);
-        formEl.email.value = formState.email;
-        formEl.message.value = formState.message;
+        if (formState.email) {formEl.email.value = formState.email;} 
+    if (formState.message) {formEl.message.value = formState.message;}
         console.log(formState);
-    }
+    } 
     
 }
-
